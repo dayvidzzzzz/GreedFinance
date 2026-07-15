762d55c89ed58cfbf4301206e94771427c3475db
dayvidzzzzz <adldayvid@gmail.com>
2026-07-15 20:13:39 -0300

    first commit

---
+++ src/main/java/dysystem/com/greedfinance/GreedFinanceApplication.java
@@ -0,0 +1,13 @@
+package dysystem.com.greedfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreedFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dysystem/com/greedfinance/GreedFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dysystem.com.greedfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
