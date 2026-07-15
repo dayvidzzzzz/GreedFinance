7decaee85388cebdc27dfe0b6cbb7ec8d08fd803
dayvidzzzzz <adldayvid@gmail.com>
2026-07-15 20:58:59 -0300

    config: inicial

--- src/main/java/dysystem/com/greedfinance/GreedFinanceApplication.java
+++ src/main/java/dysystem/com/greedfinance/GreedFinanceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreedFinanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreedFinanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GreedFinanceApplication.class, args);
+    }
 
 }
