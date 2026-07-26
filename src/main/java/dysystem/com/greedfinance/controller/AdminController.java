aea0e79fcaa64663709865b3cc532272d9da2508
dayvidzzzzz <adldayvid@gmail.com>
2026-07-26 01:54:46 -0300

    feat: adicionado endpoint que retorna lista com todos tenants

--- src/main/java/dysystem/com/greedfinance/controller/AdminController.java
+++ src/main/java/dysystem/com/greedfinance/controller/AdminController.java
@@ -2,16 +2,17 @@ package dysystem.com.greedfinance.controller;
 
 import dysystem.com.greedfinance.dto.request.TenantCreateRequestDTO;
 import dysystem.com.greedfinance.dto.response.TenantResponseCreateDTO;
+import dysystem.com.greedfinance.dto.response.TenantResponseDTO;
 import dysystem.com.greedfinance.service.useCase.tenant.CreateTenantUseCase;
+import dysystem.com.greedfinance.service.useCase.tenant.FindAllTenant;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/admin")
@@ -20,9 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private final CreateTenantUseCase createTenantUseCase;
+    private final FindAllTenant findAllTenant;
 
     @PostMapping
     public ResponseEntity<TenantResponseCreateDTO> create(@RequestBody @Valid TenantCreateRequestDTO dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(createTenantUseCase.execute(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TenantResponseDTO>> findAll(){
+        return ResponseEntity.ok(findAllTenant.execute());
     }
 }
